0360468155f95a439d378677a2ab78804ebdf748
Oliver Cassagneau-Francis <ollie.francis@gmail.com>
2020-08-12 18:30:36 +0100

    add bcs1970 data and document

---
+++ data-raw/bcs70.R
@@ -0,0 +1,54 @@
+# code to import and clean the BCS1970 data
+
+# ---- load packages ----
+library(here)
+library(data.table)
+library(foreign)
+
+# data directory
+dataDir <- "data-raw/Bcs70"
+
+# list files
+bcsFilePaths <- as.list(list.files(
+  path = here(dataDir),
+  pattern = ".sav", full.names = TRUE, recursive = TRUE
+))
+
+# extra line in case not all files should be read
+files2read <- bcsFilePaths
+
+bcsListDt <- lapply(
+  files2read,
+  read.spss, to.data.frame = TRUE
+)
+
+# extract lfs wavenames
+bcsWaves <- stringr::str_extract(files2read, pattern = "[[:alnum:]-_]*.sav")
+bcsWaves <- stringr::str_sub(bcsWaves, end = -5L)
+
+# names items in list by waves
+names(bcsListDt) <- bcsWaves
+
+bcsListDt <- lapply(bcsListDt, as.data.table)
+
+# extract variable names
+bcsListLabels <- lapply(
+  bcsListDt,
+  function(dt) {
+    data.table(varNames = attributes(dt)$names,
+               varLabels = attributes(dt)$variable.labels)
+  }
+)
+
+names(bcsListLabels) <- bcsWaves
+
+# add variable for wave
+for (i in seq_along(bcsListDt)) {
+  bcsListDt[[i]][, file := bcsWaves[[i]]]
+}
+
+# save in /data
+bcs70 <- bcsListDt
+use_data(bcs70, overwrite = T)
+bcs70labels <- bcsListLabels
+use_data(bcs70labels)
