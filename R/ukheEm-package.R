190b0adf7fcf9083bd12deaa433c5dd86e8e3f3e
Oliver Cassagneau-Francis <ollie.francis@gmail.com>
2020-08-13 12:07:33 +0100

    Document package (including importing data.table)

---
+++ R/ukheEm-package.R
@@ -0,0 +1,12 @@
+# script to create package documentation
+
+#' ukheEm: A package for estimating mixture models with exclusion restrictions
+#' on education data
+#'
+#' The package includes a main function \code{progUkheEm} that estimates a
+#' mixture model via the EM algorithm, and suitable data from two UK
+#' longitudinal surveys.
+#'
+#' @import data.table
+#' @keywords internal
+"_PACKAGE"
