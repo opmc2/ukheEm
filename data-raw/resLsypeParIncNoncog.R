222be5b2633a8afed087ea71b9aac15ccae3a39a
Oliver Cassagneau-Francis <ollie.francis@gmail.com>
2021-06-24 15:24:20 +0100

    Code to run on lsype using both parinc and noncog score

---
+++ data-raw/resLsypeParIncNoncog.R
@@ -0,0 +1,36 @@
+## --------------------------------------------------------------------------- #
+## resLsypeParInc.R
+##
+## Project: UK HE EM
+## Purpose: Runs progUkheEm() on Next Steps (lsype) data and saves
+## Author: Oliver Cassagneau-Francis
+## Date: Wed Apr 07 11:45:08 2021
+## --------------------------------------------------------------------------- #
+## Notes:
+
+# load packages
+# library(ukheEm)
+load_all()
+
+# call progUkheEm() on BCS data using parental income as y1
+resLsypeParIncNoncog <- list()
+for (K in 2:20) {
+  resLsypeParIncNoncog[[K]] <- progUkheEm(
+    dt = dtLsype4Em,
+    K = K,
+    varList = c(
+      id = "NSID",
+      y1 = "logParInc",
+      y2 = "noncogScore", # combined noncognitive test score
+      w = "logWkPay",
+      z = "leaveHome", # adult life benefits: live away from home
+      d = "degree25"
+    ),
+    maxiter = 400,
+    y1cont = TRUE,
+    y1log = TRUE,
+    J = 2
+  )
+}
+
+use_data(resLsypeParIncNoncog, overwrite = TRUE)
